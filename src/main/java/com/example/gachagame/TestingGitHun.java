6bfd02f2f918f8aa42bd292432053ee89e3cbf00
MrGoldenSun <og2828@uncw.edu>
2022-04-19 01:57:30 -0400

    V1.3 TEST

---
+++ src/main/java/com/example/gachagame/TestingGitHun.java
@@ -0,0 +1,7 @@
+package com.example.gachagame;
+
+public class TestingGitHun {
+    public static void main(String[] args) {
+        System.out.println("If this is working, then the Github integration was a success :))");
+    }
+}
