403c145dddaa032450bc400423e5121983d0af82
Bryce A <93159874+BryceADK@users.noreply.github.com>
2022-04-19 17:10:08 -0400

    some updates not done yet

--- src/main/java/com/example/gachagame/ScreenController.java
+++ src/main/java/com/example/gachagame/ScreenController.java
@@ -15,8 +15,6 @@ public class ScreenController {
     @FXML
     ImageView greedyGrinner;
 
-
-
     @FXML
     void startGameAction(ActionEvent event) throws IOException {
         gameSettings.switchScenes("characterSelect.fxml");
