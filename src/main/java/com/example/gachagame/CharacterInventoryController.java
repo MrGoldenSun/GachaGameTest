91f7922b638a547be92c17dc6100ffc579656d8e
Bryce A <93159874+BryceADK@users.noreply.github.com>
2022-04-23 21:43:13 -0400

    V 1.9 Updated Inventory and gacha screen.

--- src/main/java/com/example/gachagame/CharacterInventoryController.java
+++ src/main/java/com/example/gachagame/CharacterInventoryController.java
@@ -27,7 +27,16 @@ public class CharacterInventoryController {
     private Button Stickman6;
     @FXML
     private SplitPane Stickman1Info;
-
+    @FXML
+    private SplitPane Stickman2Info;
+    @FXML
+    private SplitPane Stickman3Info;
+    @FXML
+    private SplitPane Stickman4Info;
+    @FXML
+    private SplitPane Stickman5Info;
+    @FXML
+    private SplitPane Stickman6Info;
 
     @FXML
     void openTitleScreen() throws IOException {
@@ -48,26 +57,36 @@ public class CharacterInventoryController {
     @FXML
     void pickStickman2(ActionEvent event) {
         System.out.println(2);
+        Stickman2Info.setOpacity(1);
+        Stickman2Info.setDisable(false);
     }
 
     @FXML
     void pickStickman3(ActionEvent event) {
         System.out.println(3);
+        Stickman3Info.setOpacity(1);
+        Stickman3Info.setDisable(false);
     }
 
     @FXML
     void pickStickman4(ActionEvent event) {
         System.out.println(4);
+        Stickman4Info.setOpacity(1);
+        Stickman4Info.setDisable(false);
     }
 
     @FXML
     void pickStickman5(ActionEvent event) {
         System.out.println(5);
+        Stickman5Info.setOpacity(1);
+        Stickman5Info.setDisable(false);
     }
 
     @FXML
     void pickStickman6(ActionEvent event) {
         System.out.println(6);
+        Stickman6Info.setOpacity(1);
+        Stickman6Info.setDisable(false);
     }
 
     @FXML
@@ -75,6 +94,18 @@ public class CharacterInventoryController {
         System.out.println("Closing Menu");
         Stickman1Info.setOpacity(0);
         Stickman1Info.setDisable(true);
+        Stickman2Info.setOpacity(0);
+        Stickman2Info.setDisable(true);
+        Stickman3Info.setOpacity(0);
+        Stickman3Info.setDisable(true);
+        Stickman4Info.setOpacity(0);
+        Stickman4Info.setDisable(true);
+        Stickman5Info.setOpacity(0);
+        Stickman5Info.setDisable(true);
+        Stickman6Info.setOpacity(0);
+        Stickman6Info.setDisable(true);
+
+
     }
 
 }
