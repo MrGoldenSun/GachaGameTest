f618de74f407ed3bfe50b783bf231bf3bd9cd001
MrGoldenSun <og2828@uncw.edu>
2022-04-23 23:30:38 -0400

    V 1.9 Working gacha mechanics whoop-whoop!

--- src/main/java/com/example/gachagame/StickmanRed.java
+++ src/main/java/com/example/gachagame/StickmanRed.java
@@ -2,12 +2,26 @@ package com.example.gachagame;
 
 public class StickmanRed extends CharacterCopy{
 
-    private String name;
-    private String hometown;
-    private String story;
+    private final String name = "Gorforbius The Chosen";
+    private final String hometown = "Hometown: Austin, Texas";
+    private final String story = "Gorforbius is a simple stick man with a simple stick plan. Keep on swinging at " +
+            "things until they stop moving. His favorite things include big swords, the color red, and stale bread. " +
+            "He is noteable for having an IQ of 120 and placing 4th in Science Olympiad.";
 
     public StickmanRed() {
         super(8, 1, 20, 95, 25,
                 25, "RedStickmanNoBackground.png");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public String getStory() {
+        return story;
     }
 }
